621cf2b525479d80b84319520fa8abe44af1696e
Tjoskar <mr.karlsson.oskar@gmail.com>
2019-01-12 10:28:01 +0100

    🎨

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,45 +2,42 @@ import * as express from "express";
 import * as cors from "cors";
 import * as functions from "firebase-functions";
 import { get } from "./history";
-import { updateTitles, getTitles } from './titles';
+import { updateTitles, getTitles } from "./titles";
 import { auth } from "./firebase-app";
 import { IncomingHttpHeaders } from "http";
 
-const app = express();
-
-app.use(cors({ origin: true }));
-
-app.get("/ping", (req, res) => res.send("Pong"));
-app.get("/history", async (req, res) => {
-  const uid = await getUserId(getToken(req.headers));
-  if (!uid) {
-    res.sendStatus(403);
-  } else {
+const app = express()
+  .use(cors({ origin: true }))
+  .get("/ping", (req, res) => res.send("Pong"))
+  .get("/history", async (req, res) => {
+    const uid = await getUserId(getToken(req.headers));
+    if (!uid) {
+      res.sendStatus(403);
+    } else {
+      try {
+        res.send(await get(uid, Math.min(req.query.page | 0, 0)));
+      } catch (error) {
+        console.error(error);
+        res.sendStatus(500);
+      }
+    }
+  })
+  .get("/updatetitles", async (req, res) => {
     try {
-      res.send(await get(uid, Math.min(req.query.page | 0, 0)));
+      res.send(await updateTitles());
     } catch (error) {
-      console.error(error)
+      console.error(error);
       res.sendStatus(500);
     }
-  }
-});
-app.get("/updatetitles", async (req, res) => {
-  try {
-    res.send(await updateTitles());
-  } catch (error) {
-    console.error(error)
-    res.sendStatus(500);
-  }
-});
-
-app.get("/titles", async (req, res) => {
-  try {
-    res.send(await getTitles());
-  } catch (error) {
-    console.error(error)
-    res.sendStatus(500);
-  }
-});
+  })
+  .get("/titles", async (req, res) => {
+    try {
+      res.send(await getTitles());
+    } catch (error) {
+      console.error(error);
+      res.sendStatus(500);
+    }
+  });
 
 function getUserId(token: string | null) {
   if (!token) {
